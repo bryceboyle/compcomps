bd113e0b89fe4b6e3e7a0f88f99f19e74e9038e8
Bryce Boyle <bryce@maggiewing.com>
2021-10-26 11:10:42 -0700

    display case type given house num

--- src/Components/Search/SearchPage.js
+++ src/Components/Search/SearchPage.js
@@ -11,23 +11,14 @@ class SearchPage extends React.Component {
             options : ['Name', 'Address'],
             //defaultOption : options[0],
             inputValue : "",    // just house number for now
-            SafetyInfo : undefined
+            safetyInfo : [],
+            hasSearched : false
         }
         this._onSelect = this._onSelect.bind(this);
         this._handleChange = this._handleChange.bind(this);
         this._handleSearchClick = this._handleSearchClick.bind(this);
         this._showData = this._showData.bind(this);
-    }
-    
-
-    _onSelect(){}
-
-    _showData(){
-        fetch('https://data.lacity.org/resource/2uz8-3tj3.json?address_zip=90042') // Building and Safety Code Enforcement Case
-        .then(response => response.json())
-        .then(result =>{
-            console.log(result)
-        })
+        this._handleHomeCLick = this._handleHomeCLick.bind(this);
     }
 
     _handleChange(e){
@@ -36,12 +27,58 @@ class SearchPage extends React.Component {
         })
     }
 
-    _handleSearchClick(){}
+    _handleHomeCLick(){
+        window.location.href = "/"
+    }
+
+    _onSelect(){}
+
+    _showData(){
+        const data = this.state.safetyInfo
+        if(data.length !== 0){
+            return(
+                data.map(r=>{
+                            return(
+                                <h1>{r.case_type}</h1>
+                                //<h1>helloooo</h1>
+                            );
+                        })
+            )
+        }
+        else if(this.state.hasSearched){
+            console.log('poop')
+            return(
+                <h2>No Results Found</h2>
+            )
+        }
+        
+    }
+
+    _handleSearchClick(){
+        const input = this.state.inputValue;
+        this.setState({hasSearched : true})
+        if(input !== ""){
+            fetch(`https://data.lacity.org/resource/2uz8-3tj3.json?address_house_number=${input}`) // Building and Safety Code Enforcement Case
+            .then(response => response.json())
+            .then(result =>{
+                console.log(result)
+                this.setState({safetyInfo : result})
+            })
+            // return(
+            //     this.state.safetyInfo.map(r=>{
+            //         return(
+            //             // <h1>{r.case_type}</h1>
+            //             <h1>helloooo</h1>
+            //         );
+            //     })
+            // )
+        }
+    }
 
     render(){
         return(
             <div>
-                <div>
+                <h3 onClick={this._handleHomeCLick}>Home</h3>
                 <div>
                     <h1>Landlord Search</h1>
                 </div>
@@ -51,6 +88,8 @@ class SearchPage extends React.Component {
                     <button onClick={this._handleSearchClick}> Search </button>
                     <button onClick={this._showData}>log data</button>
                 </div>
+                <div>
+                    {this._showData()}
                 </div>
             </div>
         )
